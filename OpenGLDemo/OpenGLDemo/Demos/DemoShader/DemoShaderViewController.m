50d9855ad52d65b392091d6cbac8a6e086f3ef2d
Chris Hu <icetime17@126.com>
2016-03-24 16:39:54 +0800

    OpenGLDemo：使用OpenGLES来绘制矩形的几种方式比较。

--- OpenGLDemo/OpenGLDemo/Demos/DemoShader/DemoShaderViewController.m
+++ OpenGLDemo/OpenGLDemo/Demos/DemoShader/DemoShaderViewController.m
@@ -14,12 +14,6 @@
 
 #import "ShaderOperations.h"
 
-// 定义一个Vertex结构
-typedef struct {
-    float Position[3];
-    float Color[4];
-} Vertex;
-
 @interface DemoShaderViewController ()
 
 @end
@@ -134,15 +128,24 @@ typedef struct {
 - (void)render {
     glViewport(0, 0, self.view.frame.size.width, self.view.frame.size.height);
     
-    [self renderVertices];      // 直接使用顶点数组
+//    [self renderVertices];      // 直接使用顶点数组
 //    [self renderUsingIndex];    // 使用顶点索引数组
-//    [self renderUsingVBO];      // 使用VBO
+    [self renderUsingVBO];      // 使用VBO
     
     [_eaglContext presentRenderbuffer:GL_RENDERBUFFER];
 }
 
+#pragma mark - 直接使用顶点数组，即单纯使用glDrawArrays
+
 - (void)renderVertices {
-#pragma mark - GL_TRIANGLES
+//    [self renderVertices_triangles];
+    [self renderVertices_triangle_strip];
+//    [self renderVertices_triangle_fan];
+}
+
+#pragma mark GL_TRIANGLES
+
+- (void)renderVertices_triangles {
     // 顶点数组
     const GLfloat Vertices[] = {
         -1,-1,0,// 左下，黑色
@@ -166,6 +169,8 @@ typedef struct {
     };
     
     // 纯粹使用顶点的方式，颜色与顶点要一一对应。
+    // 在shader中DestinationColor为最终要传递给OpenGLES的颜色，要使用varying，即两个顶点之间颜色平滑渐变
+    // 若不使用varying，则完全花掉。
     
     // 取出Vertices数组中的坐标点值，赋给_positionSlot
     glVertexAttribPointer(_positionSlot, 3, GL_FLOAT, GL_FALSE, 0, Vertices);
@@ -177,63 +182,82 @@ typedef struct {
     
     // 以上两个slot分别于着色器脚本中的Positon，SourceColor两个参数
     
+    // 绘制两个三角形，不复用顶点，因此需要6个顶点坐标。
+    // V0-V1-V2, V3-V4-V5
+    
+    /**
+     *  参数1：三角形组合方式
+     *  参数2：从顶点数组的哪个offset开始
+     *  参数3：顶点个数6个
+     */
     glDrawArrays(GL_TRIANGLES, 0, 6);
-
-    
-#pragma mark - GL_TRIANGLE_STRIP
-//    // 顶点数组
-//    const GLfloat Vertices[] = {
-//        -1,-1,0,// 左下，黑色
-//        1,-1,0, // 右下，红色
-//        -1,1,0, // 左上，蓝色
-//        1,1,0,  // 右上，绿色
-//    };
-//    
-//    // 颜色数组
-//    const GLfloat Colors[] = {
-//        0,0,0,1, // 左下，黑色
-//        1,0,0,1, // 右下，红色
-//        0,0,1,1, // 左上，蓝色
-//        0,1,0,1, // 右上，绿色
-//    };
-//
-//    // 取出Vertex结构体的Position，赋给_positionSlot
-//    glVertexAttribPointer(_positionSlot, 3, GL_FLOAT, GL_FALSE, 0, Vertices);
-//    glEnableVertexAttribArray(_positionSlot);
-//    
-//    // Vertex结构体，偏移3个float的位置，即是Color值
-//    glVertexAttribPointer(_colorSlot, 4, GL_FLOAT, GL_FALSE, 0, Colors);
-//    glEnableVertexAttribArray(_colorSlot);
-//    
-//    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
-    
-#pragma mark - GL_TRIANGLE_FAN
-//    // 顶点数组
-//    const GLfloat Vertices[] = {
-//        -1,1,0, // 左上，蓝色
-//        -1,-1,0,// 左下，黑色
-//        1,-1,0, // 右下，红色
-//        1,1,0,  // 右上，绿色
-//    };
-//
-//    // 颜色数组
-//    const GLfloat Colors[] = {
-//        0,0,1,1, // 左上，蓝色
-//        0,0,0,1, // 左下，黑色
-//        1,0,0,1, // 右下，红色
-//        0,1,0,1, // 右上，绿色
-//    };
-//
-//    // 取出Vertex结构体的Position，赋给_positionSlot
-//    glVertexAttribPointer(_positionSlot, 3, GL_FLOAT, GL_FALSE, 0, Vertices);
-//    glEnableVertexAttribArray(_positionSlot);
-//
-//    // Vertex结构体，偏移3个float的位置，即是Color值
-//    glVertexAttribPointer(_colorSlot, 4, GL_FLOAT, GL_FALSE, 0, Colors);
-//    glEnableVertexAttribArray(_colorSlot);
-//
-//    glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
 }
+    
+#pragma mark GL_TRIANGLE_STRIP
+
+- (void)renderVertices_triangle_strip {
+    // 顶点数组
+    const GLfloat Vertices[] = {
+        -1,-1,0,// 左下，黑色
+        1,-1,0, // 右下，红色
+        -1,1,0, // 左上，蓝色
+        1,1,0,  // 右上，绿色
+    };
+    
+    // 颜色数组
+    const GLfloat Colors[] = {
+        0,0,0,1, // 左下，黑色
+        1,0,0,1, // 右下，红色
+        0,0,1,1, // 左上，蓝色
+        0,1,0,1, // 右上，绿色
+    };
+
+    // 取出Vertices数组中的坐标点值，赋给_positionSlot
+    glVertexAttribPointer(_positionSlot, 3, GL_FLOAT, GL_FALSE, 0, Vertices);
+    glEnableVertexAttribArray(_positionSlot);
+    
+    // 取出Colors数组中的每个坐标点的颜色值，赋给_colorSlot
+    glVertexAttribPointer(_colorSlot, 4, GL_FLOAT, GL_FALSE, 0, Colors);
+    glEnableVertexAttribArray(_colorSlot);
+    
+    // 绘制两个三角形，复用两个顶点，因此只需要四个顶点坐标
+    // V0-V1-V2, V1-V2-V3
+    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+}
+
+#pragma mark GL_TRIANGLE_FAN
+
+- (void)renderVertices_triangle_fan {
+    // 顶点数组
+    const GLfloat Vertices[] = {
+        -1,1,0, // 左上，蓝色
+        -1,-1,0,// 左下，黑色
+        1,-1,0, // 右下，红色
+        1,1,0,  // 右上，绿色
+    };
+
+    // 颜色数组
+    const GLfloat Colors[] = {
+        0,0,1,1, // 左上，蓝色
+        0,0,0,1, // 左下，黑色
+        1,0,0,1, // 右下，红色
+        0,1,0,1, // 右上，绿色
+    };
+
+    // 取出Vertices数组中的坐标点值，赋给_positionSlot
+    glVertexAttribPointer(_positionSlot, 3, GL_FLOAT, GL_FALSE, 0, Vertices);
+    glEnableVertexAttribArray(_positionSlot);
+
+    // 取出Colors数组中的每个坐标点的颜色值，赋给_colorSlot
+    glVertexAttribPointer(_colorSlot, 4, GL_FLOAT, GL_FALSE, 0, Colors);
+    glEnableVertexAttribArray(_colorSlot);
+
+    // 绘制两个三角形，复用两个顶点，因此只需要四个顶点坐标
+    // V0-V1-V2, V0-V2-V3
+    glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
+}
+
+#pragma mark - 使用顶点索引数组，使用glDrawElements
 
 - (void)renderUsingIndex {
     // 顶点数组
@@ -252,24 +276,51 @@ typedef struct {
         0,1,0,1, // 右上，绿色
     };
     
-    // 索引数组
+    // 索引数组，指定好了绘制三角形的方式
+    // 与glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);一样。
     const GLubyte Indices[] = {
         0,1,2, // 三角形0
         1,2,3  // 三角形1
     };
     
-    // 取出Vertex结构体的Position，赋给_positionSlot
+    // 取出Vertices数组中的坐标点值，赋给_positionSlot
     glVertexAttribPointer(_positionSlot, 3, GL_FLOAT, GL_FALSE, 0, Vertices);
     glEnableVertexAttribArray(_positionSlot);
     
-    // Vertex结构体，偏移3个float的位置，即是Color值
+    // 注意，未使用VBO时，glVertexAttribPointer的最后一个参数是指向对应数组的指针。
+    // 取出Colors数组中的每个坐标点的颜色值，赋给_colorSlot
     glVertexAttribPointer(_colorSlot, 4, GL_FLOAT, GL_FALSE, 0, Colors);
     glEnableVertexAttribArray(_colorSlot);
     
+    /**
+     *  参数1：三角形组合方式
+     *  参数2：索引数组中的元素个数，即6个元素，才能绘制矩形
+     *  参数3：索引数组中的元素类型
+     *  参数4：索引数组
+     */
+    // 注意，未使用VBO时，glDrawElements的最后一个参数是指向对应索引数组的指针。
     glDrawElements(GL_TRIANGLES, sizeof(Indices)/sizeof(Indices[0]), GL_UNSIGNED_BYTE, Indices);
+    
+    /**
+     *  结论：
+     *  不管使用哪种方式，顶点和颜色两个数组一定要一一对应。
+     *  glDrawArrays:
+     *  glDrawElements: 引入了索引，则很方便地实现顶点的复用。
+     *
+     *  在每个vertex上调用我们的vertex shader，以及每个像素调用fragment shader
+     *  相比glDrawArray, 使用顶点索引数组可减少存储和绘制重复顶点的资源消耗
+     */
 }
 
+#pragma mark - 使用VBO，Vertex Buffer Object
+
 - (void)renderUsingVBO {
+    // 定义一个Vertex结构, 其中包含了坐标和颜色
+    typedef struct {
+        float Position[3];
+        float Color[4];
+    } Vertex;
+    
     // 顶点数组
     const Vertex Vertices[] = {
         {{-1,-1,0}, {0,0,0,1}},// 左下，黑色
@@ -278,13 +329,14 @@ typedef struct {
         {{1,1,0}, {0,1,0,1}},  // 右上，绿色
     };
     
-    // index数组
+    // 索引数组
     const GLubyte Indices[] = {
         0,1,2, // 三角形0
         1,2,3  // 三角形1
     };
     
     // setup VBOs
+    
     // GL_ARRAY_BUFFER用于顶点数组
     GLuint vertexBuffer;
     glGenBuffers(1, &vertexBuffer);
@@ -293,7 +345,7 @@ typedef struct {
     // 给VBO传递数据
     glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
     
-    // GL_ELEMENT_ARRAY_BUFFER用于顶点数组对应的indices
+    // GL_ELEMENT_ARRAY_BUFFER用于顶点数组对应的Indices，即索引数组
     GLuint indexBuffer;
     glGenBuffers(1, &indexBuffer);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
@@ -303,15 +355,23 @@ typedef struct {
     glVertexAttribPointer(_positionSlot, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), 0);
     glEnableVertexAttribArray(_positionSlot);
     
+    // 注意，未使用VBO时，glVertexAttribPointer的最后一个参数是指向对应数组的指针。
+    // 但是，当使用VBO时，glVertexAttribPointer的最后一个参数是要获取的参数在GL_ARRAY_BUFFER（每一个Vertex）的偏移量
     // Vertex结构体，偏移3个float的位置，即是Color值
     glVertexAttribPointer(_colorSlot, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid *)(sizeof(float) * 3));
     glEnableVertexAttribArray(_colorSlot);
     
-    // 在每个vertex上调用我们的vertex shader，以及每个像素调用fragment shader
-    // 相比glDrawArray, 使用顶点索引数组可减少存储和绘制重复顶点的资源消耗
-    // 使用VBO时，参数4表示索引数据在VBO（GL_ELEMENT_ARRAY_BUFFER）中的偏移量
-    glDrawElements(GL_TRIANGLE_STRIP, sizeof(Indices)/sizeof(Indices[0]), GL_UNSIGNED_BYTE, 0);
     // glDrawArrays(GL_TRIANGLE_STRIP, 0, 4); // 使用glDrawArrays也可绘制
+    
+    /**
+     *  参数1：三角形组合方式
+     *  参数2：索引数组中的元素个数，即6个元素，才能绘制矩形
+     *  参数3：索引数组中的元素类型
+     *  参数4：索引数组在GL_ELEMENT_ARRAY_BUFFER（索引数组）中的偏移量
+     */
+    // 注意，未使用VBO时，glDrawElements的最后一个参数是指向对应索引数组的指针。
+    // 但是，当使用VBO时，参数4表示索引数据在VBO（GL_ELEMENT_ARRAY_BUFFER）中的偏移量
+    glDrawElements(GL_TRIANGLE_STRIP, sizeof(Indices)/sizeof(Indices[0]), GL_UNSIGNED_BYTE, 0);
 }
 
 @end
